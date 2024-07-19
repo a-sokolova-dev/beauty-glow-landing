385b7f8268bf25241da58eb9c85d0a8f8bbc8429
Anna Sokolova <a.sokolova.dev@gmail.com>
2024-07-19 21:41:22 +0300

    feat: add testimonials section

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import './scss/style.scss'
 
-const btnHamburger = document.querySelector('#btnHamburger')
+const btnHamburger = document.querySelector('#btn_hamburger')
 const body = document.querySelector('body')
 const header = document.querySelector('.header')
 const fadeElems = document.querySelectorAll('.has-fade')
@@ -23,4 +23,20 @@ btnHamburger.addEventListener('click', function () {
       element.classList.add('fade-in')
     })
   }
+})
+
+const list = document.querySelector('.carousel')
+const btnPrevious = document.querySelector('#btn_previous')
+const btnNext = document.querySelector('#btn_next')
+
+// We'll use the width of one of the items to decide how many pixels we want our carousel to scroll.
+const item = document.querySelector('.item')
+const itemWidth = item.offsetWidth
+
+btnPrevious.addEventListener('click', function () {
+  list.scrollBy({ behavior: 'smooth', left: -itemWidth })
+})
+
+btnNext.addEventListener('click', function () {
+  list.scrollBy({ behavior: 'smooth', left: itemWidth })
 })
