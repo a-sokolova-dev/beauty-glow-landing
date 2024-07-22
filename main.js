cf262643aac82931fe4cbc8bc096b09a56f7aca1
Anna Sokolova <a.sokolova.dev@gmail.com>
2024-07-22 11:44:39 +0300

    chore: lighthouse mobile fixes

--- main.js
+++ main.js
@@ -40,3 +40,14 @@ btnPrevious.addEventListener('click', function () {
 btnNext.addEventListener('click', function () {
   list.scrollBy({ behavior: 'smooth', left: itemWidth })
 })
+
+let domReady = (cb) => {
+  document.readyState === 'interactive' || document.readyState === 'complete'
+    ? cb()
+    : document.addEventListener('DOMContentLoaded', cb);
+};
+
+domReady(() => {
+  // Display body when DOM is loaded
+  document.body.style.visibility = 'visible';
+});
