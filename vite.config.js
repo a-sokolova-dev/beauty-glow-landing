61affc18461a415eb5cdf8540807090a81025ab0
Anna Sokolova <a.sokolova.dev@gmail.com>
2024-07-22 10:34:30 +0300

    chore: set base url in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import autoprefixer from "autoprefixer";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: '/beauty-glow-landing/',
   css: {
     postcss: {
       plugins: [autoprefixer()],
